4956c7286aed89f8a046e3e88f9226540be2e077
sparksofl <gimiro42@gmail.com>
2016-06-22 02:35:24 +0300

    update pusher event

--- app/observers/task_observer.rb
+++ app/observers/task_observer.rb
@@ -1,6 +1,6 @@
 class TaskObserver < ActiveRecord::Observer
   def after_update(task)
-    Pusher.trigger('android_channel', 'my_event', {
+    Pusher.trigger('android_channel', 'test_event', {
         message: 'Hurray! You have completed your task.'
     }) if task.completed?
   end
